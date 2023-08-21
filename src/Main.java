24ef8ca0288fff80888ba4662e3447778eccc12c
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-08-21 15:34:02 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,6 @@ public class Main {
         Person mom = new PersonBuilder().setName("Анна").setSurname("Вольф").setAge(31).setAddress("Сидней").build();
         Person son = mom.newChildBuilder().setName("Антошка").build();
         System.out.println("У " + mom + " есть сын, " + son);
-
-        if (mom.hasAge()) {
-            mom.happyBirthday();
-            System.out.println("Прошло день рождение мымы, ей теперь " + mom.getAge().getAsInt());
-        }
         try {
             // Не хватает обяхательных полей
             new PersonBuilder().setName("Анна").build();
